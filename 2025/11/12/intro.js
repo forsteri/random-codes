f9067d9c2aefc23645b2e4e6843360475bcf018e
forsteri <forza0720@gmail.com>
2025-11-12 11:55:14 +0000

    intro.jsとintro.pyを新規作成し、自己紹介と目標を表示するコードを追加

---
+++ 2025/11/12/intro.js
@@ -0,0 +1,8 @@
+// intro.js
+// 2025-11-12
+
+const name = "forsteri";
+const goal = "コードを書く";
+
+console.log(`Hello, my name is ${name}.`);
+console.log(`Today, My goal is ${goal}.`);
---
+++ 2025/11/12/intro.py
@@ -0,0 +1,6 @@
+
+name = "forsteri"
+goal = "コードを書く"
+
+print(f"Hello, my name is {name}.")
+print(f"Today, My goal is {goal}.")
