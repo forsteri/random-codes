c1327e475968e41845e8b8e59a48fdeebf85122e
forsteri <forza0720@gmail.com>
2025-11-13 13:30:38 +0000

    intro.goを新規作成し、自己紹介と目標を表示するコードを追加

---
+++ 2025/11/12/intro.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	name := "forsteri"
+	goal := "Write short code"
+
+	fmt.Printf("Hello, My name is %s. \n", name)
+	fmt.Printf("Today, My goal is %s. \n", goal)
+}
